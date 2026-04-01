32681f57cdec2a8a86a9ad8e101bd28afede36fc
hsalf-flash <vvzz346@gmail.com>
2026-04-01 19:22:29 +0100

    Minor routing fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rodio346.github.io',
-  base: '/Porfolio/',
+  base: '/Portfolio/',
   output: 'static',
 });
